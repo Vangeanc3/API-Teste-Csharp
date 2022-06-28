646dc66395dff8290551ef6222efea6b0b544102
Vangeanc3 <ismaelmartins919@gmail.com>
2022-06-28 17:12:32 -0300

    primeira versao da api teste

---
+++ API3/Models/Filme.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API3.Models
+{
+    public class Filme
+    {
+        public string Nome { get; set; }
+        public  string Diretor { get; set; }
+        public string Genero { get; set; }
+    }
+}
